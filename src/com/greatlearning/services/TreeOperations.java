496a39023362272432432210f814080b8b26a0c7
painagesh <riverjazmine@gmail.com>
2022-02-06 02:49:45 +0530

    DSA project commit 2

--- src/com/greatlearning/services/TreeOperations.java
+++ src/com/greatlearning/services/TreeOperations.java
@@ -1,59 +1,60 @@
 package com.greatlearning.services;
 
 public class TreeOperations {
-	
+
 	public static Node prevNode = null;
 	public static Node headNode = null;
-	
+
 	// Function to to flatten the binary
-		// search tree into a skewed tree in
-		// increasing order
-		public void flattenBTToSkewed(Node root)
+	// search tree into a skewed tree in
+	// increasing order
+	public void flattenBTToSkewed(Node root)
+	{
+
+		// Base Case
+		if(root == null)
 		{
-
-			// Base Case
-			if(root == null)
-			{
-				return;
-			}
-
-			flattenBTToSkewed(root.left);
-
-			Node rightNode = root.right;
-			//Node leftNode = root.left;
-
-			// Condition to check if the root Node
-			// of the skewed tree is not defined
-			if(headNode == null)
-			{
-				headNode = root;
-				root.left = null;
-				prevNode = root;
-			}
-			else
-			{
-				prevNode.right = root;
-				root.left = null;
-				prevNode = root;
-			}
-
-			flattenBTToSkewed(rightNode);
-
+			return;
 		}
-	
-		
-		public static void traverseRightSkewed(Node root)
+
+		flattenBTToSkewed(root.left);
+
+		Node rightNode = root.right;
+		//Node leftNode = root.left;
+
+		// Condition to check if the root Node
+		// of the skewed tree is not defined
+		if(headNode == null)
 		{
-			if(root == null)
-			{
-				return;
-			}
-			System.out.print(root.val + " ");
-			traverseRightSkewed(root.right);	
+			headNode = root;
+			root.left = null;
+			prevNode = root;
 		}
-		
-		public void displayTree ()
+		else
 		{
-			traverseRightSkewed (headNode);
+			prevNode.right = root;
+			root.left = null;
+			prevNode = root;
 		}
+
+		flattenBTToSkewed(rightNode);
+
+	}
+
+	//Function to traverse the skewed tree 
+	public static void traverseRightSkewed(Node root)
+	{
+		if(root == null)
+		{
+			return;
+		}
+		System.out.print(root.val + " ");
+		traverseRightSkewed(root.right);	
+	}
+
+	//Display function to print the skewed tree
+	public void displayTree ()
+	{
+		traverseRightSkewed (headNode);
+	}
 }
